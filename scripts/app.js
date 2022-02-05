a886cff7da4de37b6322d8f7fe660a34903dc999
Shushanik Simonyan <43413082+lilysimon351@users.noreply.github.com>
2022-02-05 13:28:53 +0400

    kinds popup slider; popup call from popup

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,11 @@
+// collect gallery images
+let slides = [],
+    slidesWr = document.querySelectorAll('.kind-gal__slider .swiper-wrapper');
+slidesWr.forEach(item => {
+    slides.push(item.innerHTML)
+})
+
+// works slider
 const workSlider = new Swiper('.work__slider', {
     loop: true,
     pagination: {
@@ -15,6 +23,7 @@ const workSlider = new Swiper('.work__slider', {
     effect: 'fade',
 });
 
+// reviews slider
 const reviewSlider = new Swiper('.review__slider', {
     loop: true,
 
@@ -32,22 +41,54 @@ const reviewSlider = new Swiper('.review__slider', {
     centeredSlides: true,
     effect: 'fade',
 });
-const gallerySlider = new Swiper('.gallery__slider', {
+
+// kinds images slider
+const gallerySlider = new Swiper('.kind-gal__slider', {
     loop: true,
 
     pagination: {
-        el: '.gallery__pag',
+        el: '.kind-gal__pag',
         clickable: true
     },
 
     navigation: {
-        nextEl: '.gallery__next',
-        prevEl: '.gallery__prev',
+        nextEl: '.kind-gal__next',
+        prevEl: '.kind-gal__prev',
     },
     slidesPerView: 1,
     spaceBetween: 10,
     centeredSlides: true,
 });
+
+// initilize popup kinds images slider
+function initGalleries() {
+    const thumbsSlider = new Swiper('.popup-overlay .kind__thumbs', {
+        loop: true,
+        slidesPerView: 3,
+        spaceBetween: 0,
+        freeMode: true,
+        watchSlidesProgress: true,
+        direction: 'vertical'
+    });
+    const popupGallerySlider = new Swiper('.popup-overlay .popup-gal__slider', {
+        loop: true,
+
+        pagination: {
+            el: '.popup-overlay .popup-gal__pag',
+            clickable: true
+        },
+
+        navigation: {
+            nextEl: '.popup-overlay .popup-gal__next',
+            prevEl: '.popup-overlay .popup-gal__prev',
+        },
+        slidesPerView: 1,
+        spaceBetween: 0,
+        thumbs: {
+            swiper: thumbsSlider,
+        },
+    });
+}
 
 // YT
 let videoCont = document.querySelector('.feedback__video');
@@ -66,19 +107,41 @@ let popups = document.querySelectorAll('.open-popup'),
 if (popups) {
     popups.forEach(item => {
         item.addEventListener('click', (event) => {
-            popupName = item.getAttribute('data-popup');
             event.preventDefault();
-            overlay.classList.add('popup-overlay--opened');
-            overlay.innerHTML = "";
-            overlay.appendChild(document.querySelector(`.${popupName}`).cloneNode(true));
+            makePopupFrom(item);
             overlay.querySelector('.popup__body').addEventListener('click', (event) => {
                 if (event.target.className == 'popup__close') { closePopup() }
                 event.stopPropagation();
             })
+
         })
     });
 
 }
+// make popup cont
+function makePopupFrom(item) {
+    let popupCont,
+        popupName = item.getAttribute('data-popup');
+    overlay.classList.add('popup-overlay--opened');
+    overlay.innerHTML = "";
+    popupCont = document.querySelector(`.${popupName}`).cloneNode(true);
+    overlay.appendChild(popupCont);
+    if (popupName == 'popup__gallery') {
+        let parentItem = item.parentNode.parentNode;
+        childIndex = Array.prototype.indexOf.call(parentItem.parentNode.children, parentItem);
+
+        popupCont.querySelector('.kind__thumbs-wr').innerHTML = slides[childIndex];
+        popupCont.querySelector('.popup-gal__slider-wr').innerHTML = slides[childIndex];
+        popupCont.querySelector('.popup__gal-body').appendChild(item.parentNode.cloneNode(true));
+
+        initGalleries();
+        let popupBtn = popupCont.querySelector('.kind-popup__btn');
+        popupBtn.addEventListener('click', (event) => {
+            makePopupFrom(popupBtn);
+        })
+    }
+}
+// close popup
 overlay.addEventListener('click', (event) => {
     closePopup()
 })
